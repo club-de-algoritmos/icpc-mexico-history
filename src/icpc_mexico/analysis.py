d8baf809e01e4d9cc619e7f86512438d5313e742
Moises Osorio <m@osr.io>
2023-01-24 20:56:56 -0700

    Improve team ranking.
    It's very subjective, but I think it's a good ordering.

--- src/icpc_mexico/analysis.py
+++ src/icpc_mexico/analysis.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
-from typing import List, Dict, TextIO, Set, Callable, Optional, Union, Protocol
+from typing import List, Dict, TextIO, Set, Callable, Optional, Union, Protocol, Tuple
 
-from icpc_mexico.data import FinishedContest, ContestType, SchoolCommunity, School, MEXICO
+from icpc_mexico.data import FinishedContest, ContestType, SchoolCommunity, School, MEXICO, TeamResult
 from icpc_mexico.queries import get_by_type, get_best_by_school, get_school
 
 
@@ -136,11 +136,19 @@ def _analyze_school(school: School, all_contests: List[FinishedContest], write_l
     write_line()
 
 
+TeamRank = Tuple[float, FinishedContest, TeamResult]
+
+
 def _analyze_team_rank(all_contests: List[FinishedContest], write_line: WriteLineFunction) -> None:
-    teams = []
+    honorable_teams: List[TeamRank] = []
+    high_teams: List[TeamRank] = []
     for contest in all_contests:
         if contest.type != ContestType.WORLD:
             continue
+
+        last_rank = 0
+        for team in contest.team_results:
+            last_rank = max(last_rank, team.rank)
 
         team_count = len(contest.team_results)
         for team in contest.team_results:
@@ -148,16 +156,28 @@ def _analyze_team_rank(all_contests: List[FinishedContest], write_line: WriteLin
                 continue
 
             percentile = (team_count - team.rank) / (team_count - 1)
-            teams.append((percentile, contest, team))
+            team_values = (percentile, contest, team)
+            if team.rank == last_rank or percentile < 0.5:
+                honorable_teams.append(team_values)
+            else:
+                high_teams.append(team_values)
 
-    def team_sort(values):
-        percentile, contest, team = values
-        return (1 - percentile, team.rank, -team.problems_solved, contest.year, team.name)
-    teams.sort(key=team_sort)
+    def team_sort(team_rank: TeamRank) -> Tuple:
+        percentile, contest, team = team_rank
+        return 1 - percentile, team.rank, -team.problems_solved, contest.year, team.name
+    honorable_teams.sort(key=team_sort)
+    high_teams.sort(key=team_sort)
+
+    def print_team(team_rank: TeamRank) -> None:
+        percentile, contest, team = team_rank
+        perc = round(percentile * 100)
+        write_line(f'    {perc}% {team.name} ({team.institution}), '
+                   f'solved {team.problems_solved} in {contest.year+1}, getting place {team.rank}')
 
     write_line('Ranking of Mexican teams in World Finals:')
-    for (percentile, contest, team) in teams:
-        perc = round(percentile * 100)
-        write_line(f'  {perc}% {team.name} ({team.institution}), '
-                   f'solved {team.problems_solved} in {contest.year+1}, getting place {team.rank}')
-        print(team)
+    write_line('  Ranked in the top half:')
+    for team in high_teams:
+        print_team(team)
+    write_line('  With honorable mention (solving at least one problem):')
+    for team in honorable_teams:
+        print_team(team)
